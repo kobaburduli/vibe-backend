43f572c9446bae26e27068623bb08073311def76
TatoMartoleki <tatomartoleki31@gmail.com>
2024-07-19 16:14:13 +0400

    fix: app-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'T599310631t',
+      password: '1234',
       database: 'authordatabase',
       autoLoadEntities: true,
       synchronize: true,
