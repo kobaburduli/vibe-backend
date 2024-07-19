a901d71ae634049ba2c414fcdc2fdcc103791c08
TatoMartoleki <tatomartoleki31@gmail.com>
2024-07-19 16:12:21 +0400

    add: dates

--- src/album/entities/album.entity.ts
+++ src/album/entities/album.entity.ts
@@ -1,19 +1,35 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class AlbumEntity {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({type: 'varchar'})
-    title: string;
+  @Column({ type: 'varchar' })
+  title: string;
 
-    @Column({type: 'varchar'})
-    releaseDate: string;
+  @Column({ type: 'varchar' })
+  releaseDate: string;
 
-    @Column({type: 'simple-array'})
-    musics: string[];
-    
-    @Column({type: 'int'})
-    artistId: number;
+  @Column({ type: 'simple-array' })
+  musics: string[];
+
+  @Column({ type: 'int' })
+  artistId: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
 }
