fecce7e0b6d0ea16bba399535e6059e8ef03fe36
TatoMartoleki <tatomartoleki31@gmail.com>
2024-07-22 17:29:03 +0400

    fix: conflict

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthorModule } from './author/author.module';
-import { MusicModule } from './music/music.module';
-import { AlbumModule } from './album/album.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     AuthorModule,
-    MusicModule,
-    AlbumModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
